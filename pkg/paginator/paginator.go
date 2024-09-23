f2b40fd5a61aeaa16830675876fe33992df869b9
liningning <ozz@grizzlychina.com>
2024-09-23 10:24:38 +0800

    feat: 添加分页器

---
+++ pkg/paginator/paginator.go
@@ -0,0 +1,36 @@
+package paginator
+
+import (
+	"gorm.io/gorm"
+)
+
+type Paginator struct {
+	Index int // 页码
+	Size  int // 每页数量
+}
+
+func New(index, size int) *Paginator {
+	if index <= 0 {
+		index = 1
+	}
+	if size <= 0 {
+		size = 10
+	}
+	return &Paginator{
+		Index: index,
+		Size:  size,
+	}
+}
+
+func (p *Paginator) Offset() int {
+	if p.Index <= 1 {
+		return 0
+	}
+	return (p.Index - 1) * p.Size
+}
+
+func (p *Paginator) AddInterceptor() func(db *gorm.DB) *gorm.DB {
+	return func(db *gorm.DB) *gorm.DB {
+		return db.Offset(p.Offset()).Limit(p.Size)
+	}
+}
